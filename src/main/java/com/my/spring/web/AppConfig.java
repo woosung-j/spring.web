bcc6a8294aa59750bf59af364ca0628be6807d2f
Woosung-j <developer.jws@gmail.com>
2022-09-27 12:17:46 +0900

    설정 - main.jsp

---
+++ src/main/java/com/my/spring/web/AppConfig.java
@@ -0,0 +1,15 @@
+package com.my.spring.web;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class AppConfig implements WebMvcConfigurer {
+
+	@Override
+	public void addViewControllers(ViewControllerRegistry registry) {
+		registry.addViewController("/").setViewName("ch01/main");
+	}
+	
+}
