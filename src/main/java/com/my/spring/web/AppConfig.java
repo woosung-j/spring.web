93d9ddd27ecbd529be2e6bb562ccc67e57d8e7fc
Woosung-j <developer.jws@gmail.com>
2022-09-30 10:49:00 +0900

    resource handler

--- src/main/java/com/my/spring/web/AppConfig.java
+++ src/main/java/com/my/spring/web/AppConfig.java
@@ -1,15 +1,19 @@
 package com.my.spring.web;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class AppConfig implements WebMvcConfigurer {
-
 	@Override
 	public void addViewControllers(ViewControllerRegistry registry) {
 		registry.addViewController("/").setViewName("ch01/main");
 	}
-	
+
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry.addResourceHandler("res/**").addResourceLocations("WEB-INF/res/");
+	}
 }
---
+++ src/main/webapp/WEB-INF/view/ch03/ex01/res.jsp
@@ -0,0 +1,9 @@
+<%@ page language='java' contentType='text/html; charset=UTF-8'
+    pageEncoding='UTF-8'%>
+<%@ taglib prefix='c' uri='http://java.sun.com/jsp/jstl/core' %>
+<script src='/res/alert.js'></script>
+<link rel='stylesheet' href='<c:url value="/res/box.css"/>'/>
+
+<img src='<c:url value="/res/duke.gif"/>'/>
+
+<div></div>
