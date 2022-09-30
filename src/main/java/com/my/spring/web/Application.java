1b8d007fb682c48c864ebd49f9df615db7ecd576
Woosung-j <developer.jws@gmail.com>
2022-09-30 16:12:25 +0900

    mybatis-spring test

--- src/main/java/com/my/spring/web/Application.java
+++ src/main/java/com/my/spring/web/Application.java
@@ -1,13 +1,14 @@
 package com.my.spring.web;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.my.spring.web.ch04.dao.map")
+//@MapperScan("com.my.spring.web.**.dao.map")
 public class Application {
-
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
-	
 }
